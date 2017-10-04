893e0ff4957272ecfa40fd74eeeee6a4358ac749
huangrunlai <huangrunlai@gmail.com>
2017-10-04 11:37:31 +1100

    Update question1.php

--- question1.php
+++ question1.php
@@ -1,11 +1,15 @@
-for ($row=1;$row<=10;$row++)
-{
-  $len=$row;
-  if($row>=6)
-    $len=11-$row;
-  for ($col=1;$col<=$len;$col++)
+<?php
+  for ($row=1;$row<=10;$row++)
   {
-    echo "*";
+   $len=$row;
+   if($row>=6)
+   {
+     $len=11-$row;
+   }
+   for ($col=1;$col<=$len;$col++)
+   {
+      echo "*";
+   }
+   echo "<br>";
   }
-  echo "<br>";
-}
+?>
